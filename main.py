607c5d27337e8ad4be5f35d117be3bdb6e2ee9f7
JulimPark <100160055+JulimPark@users.noreply.github.com>
2023-07-25 15:59:43 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import pandas as pd
 import streamlit as st
 
 
-# df = pd.DataFrame(pd.read_csv('wholefood.csv'))
 df = pd.DataFrame(pd.read_csv('data1.csv'))
 
 title = "<h1 style='text-align: center;color: #FBEEAC; background-color:#1D5D9B;'>My Food History</h1>"
@@ -15,7 +14,7 @@ st.write('')
 with st.expander('섭취정보 입력'):
     eat_day = st.date_input('섭취 :blue[날짜]를 입력하세요')
     eat_time = st.time_input('섭취 :blue[시간]을 입력하세요')
-    food_list = df.식품명.to_list()
+    food_list = df.음식명.to_list()
     foot_names = st.multiselect('섭취한 음식을 입력하세요',food_list)
     quantity = st.slider('몇 인분 먹었나요?',1,20,1)
 st.divider()
@@ -23,11 +22,9 @@ st.write('')
 st.write('')
 st.write('')
 st.write('')
-df_res = pd.DataFrame({'섭취일':[],'섭취시간':[],'식품명':[],'1인분칼로리(kcal)':[],'탄수화물(g)':[],'단백질(g)':[],'지질(g)':[],'콜레스트롤(g)':[],'나트륨(g)':[]})
-# df_res = pd.DataFrame({'섭취일':[],'섭취시간':[],'식품명':[],'에너지(kcal)':[],'탄수화물(g)':[],'단백질(g)':[],'지질(g)':[],'콜레스트롤(g)':[],'나트륨(mg)':[],'식품코드':[]})
+df_res = pd.DataFrame({'섭취일':[],'섭취시간':[],'음식명':[],'1인분칼로리(kcal)':[],'탄수화물(g)':[],'단백질(g)':[],'지방(g)':[],'콜레스트롤(g)':[],'식이섬유(g)':[],'나트륨(g)':[]})
 for i in foot_names:
-    ddf = df[df['식품명']==i].loc[:,['식품명','1인분칼로리(kcal)','탄수화물(g)','단백질(g)','지질(g)','콜레스트롤(g)','나트륨(g)','식품코드']]
-    # ddf = df[df['식품명']==i].loc[:,['식품명','에너지(kcal)','탄수화물(g)','단백질(g)','지질(g)','콜레스트롤(g)','나트륨(g)','식품코드']]
+    ddf = df[df['음식명']==i].loc[:,['음식명','1인분칼로리(kcal)','탄수화물(g)','단백질(g)','지방(g)','콜레스트롤(g)','식이섬유(g)','나트륨(g)']]
     df_res = pd.concat([df_res,ddf])
     df_res['섭취일'] = eat_day
     df_res['섭취시간'] = eat_time
